17d144485032b5d0aba65f878d0f66a905f1c72d
jiakairu <jiakairu@jiakairudeMacBook-Pro.local>
2025-12-16 13:20:32 +0800

    倒计时，开始+重置

---
+++ src/components/CountDown.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useRef, useState } from 'react'
+import { Button, Space } from 'antd'
+import _ from 'lodash'
+
+const initCount = 60
+const CountDown = () => {
+  const [count, setCount] = useState<number>(initCount)
+  const [isRun, setIsRun] = useState<boolean>(false)
+  const timerRef = useRef<number | null>(null)
+
+
+  useEffect(() => {
+    if (isRun) {
+      timerRef.current = window.setInterval(() => {
+        setCount(pre => {
+          if (pre === 0) {
+            timerRef.current && window.clearInterval(timerRef.current)
+            return pre
+          }
+          return pre - 1
+        })
+      }, 1000)
+    }
+    return () => {
+      timerRef.current && window.clearInterval(timerRef.current)
+    }
+  }, [isRun])
+
+  const onStart = _.debounce(() => {
+    setIsRun(true)
+  }, 500)
+
+  const onReset = () => {
+    setIsRun(false)
+    setCount(initCount)
+    timerRef.current && window.clearInterval(timerRef.current)
+  }
+  return (
+    <>
+      <h1>倒计时：{count} s</h1>
+      <Space>
+        <Button type='primary' onClick={() => onStart()} disabled={isRun}>开始</Button>
+        <Button onClick={() => onReset()}>重置</Button>
+      </Space>
+    </>
+  )
+}
+
+export default CountDown
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,20 +1,9 @@
-import { Button } from 'antd'
-import { useState } from 'react'
+import CountDown from 'src/components/CountDown'
 
 const About = () => {
-  const [count, setCount] = useState(0)
-  const onAddOne = () => {
-    setCount(count + 1)
-  }
-  const onSubOne = () => {
-    setCount(count - 1)
-  }
   return (
     <div>
-      <h1>About Page</h1>
-        <Button onClick={onAddOne}>add one</Button>
-        <Button onClick={onSubOne}>sub one</Button>
-      <div>{ count }</div>
+     <CountDown />
     </div>
   )
 }
